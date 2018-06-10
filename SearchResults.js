10fb44416d520719c3aab28bfa686f473259ff72
mfouda214 <m.fouda@robone.net>
2018-06-10 05:45:37 +0100

    Add SearchResuls Class

---
+++ SearchResults.js
@@ -0,0 +1,39 @@
+// imports the relevant modules
+'use strict';
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Image,
+  View,
+  TouchableHighlight,
+  FlatList,
+  Text,
+} from 'react-native';
+
+export default class SearchResults extends Component<{}> {
+  _keyExtractor = (item, index) => index;
+
+  _renderItem = ({item}) => {
+    return (
+      <TouchableHighlight
+        underlayColor='#dddddd'>
+        <View>
+          <Text>{item.title}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+
+  };
+
+  render() {
+    return (
+      // FlatList Similar to UITableView
+      <FlatList
+        data={this.props.listings}
+        keyExtractor={this._keyExtractor}
+        renderItem={this._renderItem}
+      />
+    );
+  }
+}
