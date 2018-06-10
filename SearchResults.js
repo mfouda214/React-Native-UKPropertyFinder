37dd6a84b527596e88502adac91347c114f8ea6a
mfouda214 <m.fouda@robone.net>
2018-06-10 05:59:05 +0100

    Add row handling

--- SearchResults.js
+++ SearchResults.js
@@ -14,16 +14,17 @@ import {
 export default class SearchResults extends Component<{}> {
   _keyExtractor = (item, index) => index;
 
-  _renderItem = ({item}) => {
-    return (
-      <TouchableHighlight
-        underlayColor='#dddddd'>
-        <View>
-          <Text>{item.title}</Text>
-        </View>
-      </TouchableHighlight>
-    );
+  _renderItem = ({item, index}) => (
+    <ListItem
+      item={item}
+      index={index}
+      onPressItem={this._onPressItem}
+    />
+  );
 
+  // _onPressItem is passed into ListItem to handle a row selection. 
+  _onPressItem = (index) => {
+    console.log("Pressed row: "+index);
   };
 
   render() {
@@ -37,3 +38,59 @@ export default class SearchResults extends Component<{}> {
     );
   }
 }
+
+class ListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.index);
+  }
+
+  render() {
+    const item = this.props.item;
+    const price = item.price_formatted.split(' ')[0];
+    return (
+      <TouchableHighlight
+        onPress={this._onPress}
+        underlayColor='#dddddd'>
+        <View>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: item.img_url }} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>{price}</Text>
+              <Text style={styles.title}
+                numberOfLines={1}>{item.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  },
+});
